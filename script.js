4d7fe76c0ac00620b2212779a05e74b2364b7878
Sampad Kumar Saha <sampadsaha5@gmail.com>
2016-03-13 15:32:15 +0530

    Added the Checkanswer functionality

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ var sudoku = new Array(9);
 for(var i=0; i<=8; i++){
     sudoku[i] = new Array(9);
 }
+function trigger(){
+    setWidthAndHeight();
+    appender();
+}
+function setWidthAndHeight(){
+    document.getElementById('container').style.height = docHeight + "px";
+    document.getElementById('centrallayer').style.height = docHeight - 115 + 'px';
+}
+
+
+
+
+
+
+
+
+
+
+
+function checkAnswers(){
+    var i = false;
+    var j = false;
+    var corrects = 0;
+    for(i=1; i<=9; i++){
+        for(j=1; j<=9; j++){
+            if(document.getElementById('puz'+i+''+j).innerHTML == sudoku[i-1][j-1]) corrects ++;
+        }
+    }
+    if(corrects == 81){
+        document.getElementById('goodbox').innerHTML = 'You did it !!';
+        alert('You did it !!');
+    }
+}
